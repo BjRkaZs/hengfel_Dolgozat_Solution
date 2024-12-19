71837ec995b1a1ede09431640243a0512d9fb993
Molnár Bence <molnar.bence.email@gmail.com>
2024-12-19 15:42:41 +0100

    second commit

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@
 * Copyright: 2024, Bajusz Réka Zsófi
 * Group: Szoft I-2-N
 * Date: 2024-12-19
-* Github: https://github.com/BjRkaZs/
+* Github: https://github.com/BjRkaZs/hengfel_Dolgozat_Solution
 * Licenc: MIT
 */
 public class App {
