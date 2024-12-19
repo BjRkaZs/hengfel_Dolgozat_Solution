f3a4824fd5e218417442aec454b9e3e265a2a9e0
Molnár Bence <molnar.bence.email@gmail.com>
2024-12-19 15:24:51 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+/*
+* File: App.java
+* Author: Bajusz Réka Zsófi
+* Copyright: 2024, Bajusz Réka Zsófi
+* Group: Szoft I-2-N
+* Date: 2024-12-19
+* Github: https://github.com/BjRkaZs/
+* Licenc: MIT
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bajusz Réka Zsófi, 2024-12-19");
+        System.out.println("Henger felület számítása:");
+        new Solution().startCalc();
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,29 @@
+/*
+* File: Solution.java
+* Author: Bajusz Réka Zsófi
+* Copyright: 2024, Bajusz Réka Zsófi
+* Group: Szoft I-2-N
+* Date: 2024-12-19
+* Github: https://github.com/BjRkaZs/
+* Licenc: MIT
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+   public void startCalc(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Sugár: ");
+    String radiusStr = sc.nextLine();
+    Double radius = Double.parseDouble(radiusStr);
+
+    System.out.print("Hossz: ");
+    String heightStr = sc.nextLine();
+    Double height = Double.parseDouble(heightStr);
+
+    Double area = 2 * Math.PI * radius * height + 2 * Math.PI * Math.pow(radius, 2);
+      
+    System.out.printf("Felszín: "+ area);
+    sc.close();
+   } 
+}
